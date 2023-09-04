03bcb910e639b6def627fb90f8185a0839910144
Uttam Shrestha <shresthauttam66@gmail.com>
2023-09-04 17:51:30 +0530

    components

---
+++ resources/views/include/booking_preview.blade.php
@@ -0,0 +1,38 @@
+
+    <!-- How to Book Ambulance -->
+    <section class="booking-preview">
+      <h1 class="main-heading">How to Book Ambulance</h1>
+      <a
+        href="#"
+        class="text-decoration-none text-white rounded-5 shadow-lg my-2 primary-cta"
+        >Download MedCab App</a
+      >
+      <div
+        class="booking-previewCarousel owl-carousel owl-theme owl-carousel-facilities"
+      >
+        <div class="item">
+          <img
+            src="asset('assets/website-images/book-ambulance-1.png')"
+            alt="screenshot of booking process"
+          />
+        </div>
+        <div class="item">
+          <img
+            src="asset('assets/website-images/book-ambulance-2.png')"
+            alt="screenshot of booking process"
+          />
+        </div>
+        <div class="item">
+          <img
+            src="asset('assets/website-images/book-ambulance-3.png')"
+            alt="screenshot of booking process"
+          />
+        </div>
+        <div class="item">
+          <img
+            src="asset('assets/website-images/book-ambulance-4.png')"
+            alt="screenshot of booking process"
+          />
+        </div>
+      </div>
+    </section>
--- resources/views/include/download_banner.blade.php
+++ resources/views/include/download_banner.blade.php
@@ -1,54 +1,54 @@
-<div class="container p-relative pt-5 d-flex justify-content-center">
-        <div class="blog-footer-banner p-relative">
-            <img class="footer-banner-img w-100" src="{{url('/assets/image/blog-bg.png')}}" /> 
-            <div class="footer-banner-content   w-100 ">
-                <h1 class="title-text text-white d-flex-center w-100">
-                    <img src="{{url('/assets/icons/white-ambu.png')}}" alt="" class="ambu-icon mr-2">
-                    Download MedCab App
-                </h1>
-                <div class="row d-flex-center w-100">
-                    <div class="col-lg-6 download-mob-img">
-                        <div class="app-img d-flex-center w-100" style="gap:40px">
-                            <img src="{{url('/assets/image/app-page-1.png')}}" alt="" class="mob-img">
-                            <img src="{{url('/assets/image/app-page-2.png')}}" alt="" class="mob-img">
-                        </div>                    
-                    </div>
-                    <div class="col-lg-5 d-flex-center flex-column gy-3  " >
-                        <div class="app-download">
-                            <h1 class="sub-title">Partner App</h1>
-                            <div class="download-btn-group">
-                                <a href="https://play.google.com/store/apps/details?id=com.medcab.partner" class="google-btn">
-                                    <img src="{{url('/assets/image/Google Play.png')}}" alt="Google Play" class="download-btn-img">
-                                </a>
-                                <a href="https://apps.apple.com/in/app/medcab-partner/id6448983664" class="google-btn">
-                                    <img src="{{url('/assets/image/App Store.png')}}" alt="Apple Store" class="download-btn-img">
-                                </a>
-                            </div>
-                        </div>
-                        <!--<div class="app-download">-->
-                        <!--    <h1 class="">Driver App</h1>-->
-                        <!--    <div class="download-btn-group">-->
-                        <!--        <a href="" class="google-btn">-->
-                        <!--            <img src="{{url('/assets/image/Google Play.png')}}" alt="Google Play" class="download-btn-img">-->
-                        <!--        </a>-->
-                        <!--        <a href="" class="google-btn">-->
-                        <!--            <img src="{{url('/assets/image/App Store.png')}}" alt="Apple Store" class="download-btn-img">-->
-                        <!--        </a>-->
-                        <!--    </div>-->
-                        <!--</div>-->
-                        <div class="app-download">
-                            <h1 class="">Consumer App</h1>
-                            <div class="download-btn-group">
-                                <a href="https://play.google.com/store/apps/details?id=com.medcab.consumer" class="google-btn">
-                                    <img src="{{url('/assets/image/Google Play.png')}}" alt="Google Play" class="download-btn-img">
-                                </a>
-                                <!--<a href="" class="google-btn">-->
-                                <!--    <img src="{{url('/assets/image/App Store.png')}}" alt="Apple Store" class="download-btn-img">-->
-                                <!--</a>-->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>   
-        </div>    
-    </div>
+ <!-- download app -->
+ <section class="ourServices d-flex flex-column align-items-center">
+     <section class="section-6 download-app d-flex flex-column align-items-center">
+         <div class="label d-flex align-items-center">
+             <div class="image me-3">
+                 <img src="../images/runningAmbulance.jpg" alt="" />
+             </div>
+             <h3 class="text-center text-white fw-bold mb-0">
+                 Download MedCab App
+             </h3>
+         </div>
+         <div class="content d-flex align-items-center">
+             <div class="phones d-flex gap-4">
+                 <img src="../images/Nothing Phone 1.png" alt="" />
+                 <img src="../images/Nothing Phone 2.png" alt="" />
+             </div>
+             <div class="downloadBtns d-flex flex-column gap-5">
+                 <div class="user d-flex flex-column gap-3">
+                     <h4 class="text-white fw-bold">MedCab App</h4>
+                     <div class="btns d-flex gap-3">
+                         <button class="btn px-0">
+                             <img src="../images/Google Play.png" alt="" />
+                         </button>
+                         <button class="btn px-0">
+                             <img src="../images/app-store.png" alt="" />
+                         </button>
+                     </div>
+                 </div>
+                 <div class="user d-flex flex-column gap-3">
+                     <h4 class="text-white fw-bold">Driver App</h4>
+                     <div class="btns d-flex gap-3">
+                         <button class="btn px-0">
+                             <img src="../images/Google Play.png" alt="" />
+                         </button>
+                         <button class="btn px-0">
+                             <img src="../images/app-store.png" alt="" />
+                         </button>
+                     </div>
+                 </div>
+                 <div class="user d-flex flex-column gap-3">
+                     <h4 class="text-white fw-bold">Partner App</h4>
+                     <div class="btns d-flex gap-3">
+                         <button class="btn px-0">
+                             <img src="../images/Google Play.png" alt="" />
+                         </button>
+                         <button class="btn px-0">
+                             <img src="../images/app-store.png" alt="" />
+                         </button>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
+ </section>
