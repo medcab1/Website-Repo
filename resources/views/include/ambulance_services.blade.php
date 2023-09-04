17fee1b64a49c7097e384904df78f1e722aa6384
Saurabh <saurabhawasthi618@gmail.com>
2023-09-04 18:41:20 +0530

    components
    added all the components of page our services

---
+++ resources/views/include/ambulance_services.blade.php
@@ -0,0 +1,102 @@
+<!-- ambulance services -->
+<section class="ourServices d-flex flex-column align-items-center">
+    
+    <section class="ambulance-services section-3 d-flex flex-column align-items-center">
+
+        <div class="label">
+                <h3 class="text-center fw-bold">Ambulance Services</h3>
+            </div>
+            <div class="content px-md-5 w-100 d-flex gap-5 mt-5 mb-5">
+                <div class="left-content w-50">
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>Basic Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>Advance Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>ICU Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>Advance Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>Transfer Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>Transfer Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>ICU Ambulance</p>
+                    </div>
+                    <div
+                        class="ambuCard justify-content-center d-flex flex-column align-items-center text-center py-4 py-sm-3 rounded-4 bg-white">
+                        <img class="" src="{{asset('assets/website-images/Component 11.png')}}" alt="ambulance" />
+                        <p>Transfer Ambulance</p>
+                    </div>
+                </div>
+                <div class="right-content w-50 rounded-4 d-flex flex-column justify-content-evenly gap-4">
+                    <div class="bg-white rounded-4 py-4 px-2">
+                        <div class="one d-flex flex-column align-items-center">
+                            <img src="{{asset('assets/website-images/Component 11.png')}}" alt="" />
+                            <h4 class="fw-bold text-center">Advance Ambulance</h4>
+                        </div>
+                        <div class="two">
+                            <p class="text-center">
+                                Medical First Responder (MFR) ambulances are the first
+                                responders to any medical emergency. They are equipped with
+                                basic medical equipments and trained medical personnel to
+                                stabilize patients until more advanced care arrives.
+                            </p>
+                        </div>
+                        <div class="three">
+                            <div
+                                class="card-three cardAmbu d-flex flex-column align-items-center justify-content-center">
+                                <img class="mb-sm-2" src="{{asset('assets/website-images/suitcase.png')}}" alt="" />
+                                <p class="text-center">Emergency Kit</p>
+                            </div>
+                            <div
+                                class="card-three cardAmbu d-flex flex-column align-items-center justify-content-center">
+                                <img class="mb-2" src="{{asset('assets/website-images/Multipara monitor.png')}}" alt="" />
+                                <p class="text-center">Emergency Kit</p>
+                            </div>
+                            <div
+                                class="card-three cardAmbu d-flex flex-column align-items-center justify-content-center">
+                                <img class="mb-2" src="{{asset('assets/website-images/oxygen.png')}}" alt="" />
+                                <p class="text-center">Emergency Kit</p>
+                            </div>
+                            <div
+                                class="card-three cardAmbu d-flex flex-column align-items-center justify-content-center">
+                                <img class="mb-2" src="{{asset('assets/website-images/stretcher.png')}}" alt="" />
+                                <p class="text-center">Emergency Kit</p>
+                            </div>
+                            <div
+                                class="card-three cardAmbu d-flex flex-column align-items-center justify-content-center">
+                                <img class="mb-2" src="{{asset('assets/website-images/wheelchair.png')}}" alt="" />
+                                <p class="text-center">Emergency Kit</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="download-button d-flex justify-content-center">
+                <a href="#" class="text-decoration-none shadow-lg my-2 primary-cta text-white">Download MedCab App</a>
+            </div>
+        </section>
+    </section>
+
+<!-- ambulance services -->
---
+++ resources/views/include/faqs.blade.php
@@ -0,0 +1,116 @@
+<!-- faqs -->
+<section class="section-5 w-100 py-5">
+            <div class="label">
+                <h3 class="text-center fw-bold mb-5">
+                    Frequently Asked Questions (FAQs)
+                </h3>
+            </div>
+            <div class="accordion px-2 px-sm-3 px-md-4 px-lg-5" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header mb-2" id="headingOne">
+                        <button class="accordion-button shadow-none collapsed rounded-4 d-flex justify-content-between"
+                            type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false"
+                            aria-controls="collapseOne">
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('assets/website-images/Q.png')}}" alt="q" />
+                                <strong>
+                                    <p class="ms-3">
+                                        What payment modes do you accept for booking an ambulance?
+                                    </p>
+                                </strong>
+                            </div>
+                            <!-- <p class="plus-icon">+</p> -->
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body d-flex align-items-start rounded-4">
+                            <img src="{{asset('assets/website-images/A.png')}}" alt="a" />
+                            <p class="ms-3">
+                                <strong>This is the first item's accordion body.</strong> It
+                                is shown by default, until the collapse plugin adds the
+                                appropriate classes that we use to style each element. These
+                                classes control the overall appearance, as well as the showing
+                                and hiding via CSS transitions. You can modify any of this
+                                with custom CSS or overriding our default variables. It's also
+                                worth noting that just about any HTML can go within the
+                                <code>.accordion-body</code>, though the transition does limit
+                                overflow.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header mb-2" id="headingTwo">
+                        <button class="accordion-button shadow-none collapsed rounded-4 d-flex justify-content-between"
+                            type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
+                            aria-controls="collapseTwo">
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('assets/website-images/Q.png')}}" alt="q" />
+                                <strong>
+                                    <p class="ms-3">
+                                        What payment modes do you accept for booking an ambulance?
+                                    </p>
+                                </strong>
+                            </div>
+                            <!-- <p class="plus-icon">+</p> -->
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body d-flex align-items-start rounded-4">
+                            <img src="{{asset('assets/website-images/A.png')}}" alt="a" />
+                            <p class="ms-3">
+                                <strong>This is the first item's accordion body.</strong> It
+                                is shown by default, until the collapse plugin adds the
+                                appropriate classes that we use to style each element. These
+                                classes control the overall appearance, as well as the showing
+                                and hiding via CSS transitions. You can modify any of this
+                                with custom CSS or overriding our default variables. It's also
+                                worth noting that just about any HTML can go within the
+                                <code>.accordion-body</code>, though the transition does limit
+                                overflow.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header mb-2" id="headingThree">
+                        <button class="accordion-button shadow-none collapsed rounded-4 d-flex justify-content-between"
+                            type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree"
+                            aria-expanded="false" aria-controls="collapseThree">
+                            <div class="d-flex align-items-center">
+                                <img src="{{asset('assets/website-images/Q.png')}}" alt="q" />
+                                <strong>
+                                    <p class="ms-3">
+                                        What payment modes do you accept for booking an ambulance?
+                                    </p>
+                                </strong>
+                            </div>
+                            <!-- <p class="plus-icon">+</p> -->
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body d-flex align-items-start rounded-4">
+                            <img src="{{asset('assets/website-images/A.png')}}" alt="a" />
+                            <p class="ms-3">
+                                <strong>This is the first item's accordion body.</strong> It
+                                is shown by default, until the collapse plugin adds the
+                                appropriate classes that we use to style each element. These
+                                classes control the overall appearance, as well as the showing
+                                and hiding via CSS transitions. You can modify any of this
+                                with custom CSS or overriding our default variables. It's also
+                                worth noting that just about any HTML can go within the
+                                <code>.accordion-body</code>, though the transition does limit
+                                overflow.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- faqs -->
