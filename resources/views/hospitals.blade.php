dbab8b09eaeefbab7b3cc74914f087e235076667
Uttam Shrestha <shresthauttam66@gmail.com>
2023-09-05 15:25:57 +0530

    Hospital Page

--- resources/views/hospitals.blade.php
+++ resources/views/hospitals.blade.php
@@ -10,7 +10,28 @@ services to hospitals with 24/7 customer support in all over India.")
 <!-- hospitals header -->
     
 <!-- hospitals header -->
-@include('include.hospital_availability_main');
+
+<section class="ourServices d-flex flex-column align-items-center">
+      <section class="section-4 grid-container-box d-flex justify-content-center">
+          <div class="facilities gap-3">
+
+                <div class="label mb-md-5 mb-sm-3">
+                  <h3 class="text-center fw-bold mb-4">
+                      Check Hospital Availability
+                  </h3>
+                  <p class="text-center">
+                      Discover the convenience of checking the availability of beds such
+                      as ICU Beds, ISU Beds, etc. in nearby hospitals effortlessly,
+                      helping you make informed decisions during critical times.
+                  </p>
+                </div>
+
+
+                @include('include.hospital_availability_main');
+
+                </div>
+      </section>
+  </section>
 
 
 @endsection
--- resources/views/include/hospital_availability_main.blade.php
+++ resources/views/include/hospital_availability_main.blade.php
@@ -3,19 +3,8 @@
   $get_hospital_data = DB::table('hospital_service_category')
     ->get();
     @endphp
-  <section class="ourServices d-flex flex-column align-items-center">
-      <section class="section-4 grid-container-box d-flex justify-content-center">
-          <div class="facilities gap-3">
-              <div class="label mb-md-5 mb-sm-3">
-                  <h3 class="text-center fw-bold mb-4">
-                      Check Hospital Availability
-                  </h3>
-                  <p class="text-center">
-                      Discover the convenience of checking the availability of beds such
-                      as ICU Beds, ISU Beds, etc. in nearby hospitals effortlessly,
-                      helping you make informed decisions during critical times.
-                  </p>
-              </div>
+  
+              
               <div class="grid-container">
                 @foreach($get_hospital_data as $key)
                   <a href="#">
@@ -29,6 +18,4 @@
                   </a>
                   @endforeach
               </div>
-          </div>
-      </section>
-  </section>
+          
